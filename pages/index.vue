3cf239831b2a8a1be1eaef3353701b7ec364e741
RodgersX <brianmawira2@gmail.com>
2022-02-22 09:50:01 +0300

    COMMIT: initial commit

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <Nav />
+    <Tutorial />
+  </div>
+</template>
+
+<script>
+import Nav from '../components/navBar.vue'
+
+export default {
+  name: 'IndexPage',
+  components: {
+    Nav,
+  },
+}
+</script>
